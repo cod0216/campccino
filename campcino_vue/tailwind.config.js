b66a8916ce8325cccf0a9acd873bbb9f760821ee
cod0216 <cod0216@nate.com>
2025-06-29 07:35:21 +0900

    style: tailwind.config modify

--- campcino_vue/tailwind.config.js
+++ campcino_vue/tailwind.config.js
@@ -1,8 +1,24 @@
-// tailwind.config.js
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        campccino: {
+          DEFAULT: "#009963",
+          bg: '#f8f9fa',
+          0: '#F5F0E5',
+          100: '#D9F0E8',
+          200: '#B0DFCF',
+          300: '#009963',
+          400: '#008A59',
+          500: '#00734A',
+          600: '#005C3B',
+          700: '#005C3B',
+          800: '#00452D',
+          900: '#003623',
+        },
+      }
+    },
   },
   plugins: [],
 };
